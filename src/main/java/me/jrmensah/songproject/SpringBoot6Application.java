9299eb01849742598d4fcbde124ed065e3ba9a55
jrmensah <jerilynmensah@gmail.com>
2017-11-08 10:43:01 -0500

    Song form app

---
+++ src/main/java/me/jrmensah/songproject/SpringBoot6Application.java
@@ -0,0 +1,12 @@
+package me.jrmensah.songproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot6Application.class, args);
+	}
+}
